e2da4d8b4b3d5ff68d55a4e90a7da27e50da8fd1
CLopez021 <108363556+CLopez021@users.noreply.github.com>
2022-11-20 03:47:50 +0000

    Implemented SQL Database

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-
 from flask import Flask, render_template, session, url_for, request
 import pandas as pd
 import json
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reviews.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#initialize the database
+db = SQLAlchemy(app)
+app.app_context().push()
+
+#create db model
+class Reviews(db.Model):
+  #each id is unique so the db knows what we're looking for
+  id = db.Column(db.Integer, primary_key = True)
+  subjectName = db.Column(db.String(200), nullable = False)
+  className = db.Column(db.String(200), nullable = False)
+  rating = db.Column(db.Integer, nullable= False)
+  dateCreated = db.Column(db.DateTime, default = datetime.utcnow)
+  #create a function to return a string when we add something
+  def __repr__(self):
+    return '<Name %r>' % self.id
 app.secret_key = "dljsaklqk24e21cjn!Ew@@dsa5"
-def stats(self):
-  print(self.file.columns.values)
-  print(f"Max Price: {max(self.file['Price'])}")
-  print(f"Lowest User Rating: {min(self.file['User Rating'])}")
-  print(f"Years of data: {max(self.file['Year'])-min(self.file['Year'])}")
-  print(f"Average Price: {self.file['Price'].mean()}")
-  print(f"Unique Authors: {len(self.file['Author'].unique())}")
-def yearlyBest(self, year):
-#     Define a function yearlyBest() that will take in the dataset and a year, and will display the Top 5 Best Sellers by the highest number of reviews.
-# Next, try to find the top 5 authors by number of books they have on this list.
-# Next, try to find the top 5 authors by total number of reviews for all of their books on the list
-# Lastly, try to find the top 5 authors by highest average review score for their books on the list
-  self.file.sort_values(by = "Reviews")
-  print(f"Top 5 Best Sellers:\n{self.file.head(5)['Name']}")
-  print(f"Top 5 Best Sellers:\n{self.file['Author']}")
-  return render_template("base.html")
 
 def serveButtons(html, content,title):
     URLS = []
@@ -44,20 +47,31 @@ def WriteReviews():
   return res
 @app.route('/ViewReviews/')
 def ViewReviews():
-  file = session.get("mainDF", None)
-  file= pd.DataFrame.from_dict(file)
-  res = serveButtons("subjects.html", file["Subject"].unique(), "Subjects")
+  #file = session.get("mainDF", None)
+  #file= pd.DataFrame.from_dict(file)
+  subjectsCursor = db.session.execute('SELECT DISTINCT subjectName FROM Reviews')
+  subjectsList = [s[0] for s in subjectsCursor.fetchall()]
+  res = serveButtons("subjects.html", subjectsList, "Subjects")
   return res
-@app.route('/ViewReviews/Submit', methods = ["POST"])
+@app.route('/WriteReviews/Submit', methods = ["GET","POST"])
 def reviewSubmit():
   Class = request.form.get("class")
   subject = request.form.get("subject")
   rating = request.form.get("rate")
+  new_review = Reviews(subjectName = subject, className = Class, rating = rating)
+  try:
+    db.session.add(new_review)
+    db.session.commit()
+    return render_template("submission.html")
+  except:
+    return render_template("submissionFailed.html")
   print(f"class:{Class}, subject:{subject}, rating:{rating}")
-  return render_template("submission.html")
 @app.route('/ViewReviews/<subject>/')
 def ViewSubject(subject):
-  file = session.get("mainDF",None)
-  file = pd.DataFrame.from_dict(file)
-  classes = file[file.Subject ==subject]["Classes"].unique()
-  return serveButtons("classes.html", classes,"Classes")
+  #file = session.get("mainDF",None)
+  #file = pd.DataFrame.from_dict(file)
+  sql = text('SELECT DISTINCT className from Reviews WHERE subjectName= :subject')
+  classesCursor = db.session.execute(sql,{"subject":subject})
+  classesList = [c[0] for c in classesCursor.fetchall()]
+  #classes = file[file.Subject ==subject]["Classes"].unique()
+  return serveButtons("classes.html", classesList,"Classes")
